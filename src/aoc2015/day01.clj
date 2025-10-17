16edecaa1cd6edaa5b409332a1f3631832bcbaeb
Steve Hill <229538+seestevecode@users.noreply.github.com>
2025-10-17 15:07:39 +0100

    Refactor 2015 Day 1

--- src/aoc2015/day01.clj
+++ src/aoc2015/day01.clj
@@ -2,12 +2,17 @@
   "Advent of Code 2015 Day 1 - Not Quite Lisp"
   (:require [clojure.string :as str]))
 
-(def ^:private input (slurp "resources/event_2015/aoc2015-d01-input.txt"))
-(def ^:private parsed (mapv {\( 1, \) -1} (str/trim-newline input)))
+(def test-input "(()))(")
+(def puzzle-input (slurp "resources/event_2015/aoc2015-d01-input.txt"))
 
-(defn part-1 [s] (reduce + s))
-(defn part-2 [s] (count (take-while (complement neg?) (reductions + 0 s))))
+(defn- parse [input] (mapv {\( 1, \) -1} (str/trim-newline input)))
 
-;; Solutions: 280 / 1797
-(str "Solutions: " (part-1 parsed) " / " (part-2 parsed))
+(defn part-1 [input] (reduce + input))
+(defn part-2
+  [input]
+  (count (take-while (complement neg?) (reductions + 0 input))))
 
+(comment
+  ;; Solutions: 280 / 1797
+  (let [input puzzle-input] ; change as required
+    (str "Solutions: " (part-1 (parse input)) " / " (part-2 (parse input)))))
