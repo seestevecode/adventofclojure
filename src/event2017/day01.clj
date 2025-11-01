88b243a2bf84ce42156bab5de1293ae44986062b
Steve Hill <229538+seestevecode@users.noreply.github.com>
2025-11-01 16:19:45 +0000

    Add 2017 day 1 solution

---
+++ src/event2017/day01.clj
@@ -0,0 +1,22 @@
+;;;; Advent of Code 2017 Day 1 - Inverse Captcha
+
+(ns event2017.day01
+  (:require [clojure.string :as str]
+            [utils.aoc :as aoc]))
+
+(def puzzle-input (aoc/get-input 2017 1))
+
+(defn- parsed [input] (map #(parse-long (str %)) (str/trim-newline input)))
+
+(defn- captcha-sum
+  [seq offset]
+  (let [seq-rotated (concat (drop offset seq) (take offset seq))]
+    (reduce + 0 (map #(if (= %1 %2) %1 0) seq seq-rotated))))
+
+(defn part-1 [input] (captcha-sum (parsed input) 1)) ; adjacent
+(defn part-2
+  [input]
+  (let [p (parsed input)] (captcha-sum p (quot (count p) 2)))) ; halfway
+
+(aoc/solve puzzle-input part-1 part-2)
+
