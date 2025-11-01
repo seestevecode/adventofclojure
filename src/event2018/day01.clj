70172511869b6851b2918e603c8102580a650a82
Steve Hill <229538+seestevecode@users.noreply.github.com>
2025-11-01 16:20:21 +0000

    Add 2018 day 1 solution

---
+++ src/event2018/day01.clj
@@ -0,0 +1,19 @@
+;;;; Advent of Code 2018 Day 1 - Chronal Calibration
+
+(ns event2018.day01
+  (:require [clojure.string :as str]
+            [utils.aoc :as aoc]))
+
+(def puzzle-input (aoc/get-input 2018 1))
+
+(defn- parsed [input] (map parse-long (str/split-lines input)))
+
+(defn- first-dup
+  [xs]
+  (reduce (fn [seen x] (if (seen x) (reduced x) (conj seen x))) #{} xs))
+
+(defn part-1 [input] (reduce + (parsed input)))
+(defn part-2 [input] (first-dup (reductions + (cycle (parsed input)))))
+
+(aoc/solve puzzle-input part-1 part-2)
+
